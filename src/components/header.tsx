2cfdf140b62e68fbaf30f595cc0603170fe8cab1
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-09-14 19:01:21 +0300

    card-tailwind

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <>
+      <h2 className="text-green-500 text-7xl py-4 border-2 border-slate-800">header</h2>
+    </>
+  )
+}
+
+export default Header
--- src/setupTests.js
+++
@@ -1,9 +0,0 @@
-import { beforeAll } from 'vitest';
-import { JSDOM } from 'jsdom';
-
-beforeAll(() => {
-    const dom = new JSDOM();
-    global.document = dom.window.document;
-    global.window = dom.window;
-    
-});
